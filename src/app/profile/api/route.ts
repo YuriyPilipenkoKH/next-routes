90b20dd8f27713567de4090d3b8dd9c7b370df74
Yuriy Pilipenko <yurik2061@gmail.com>
2024-03-06 00:27:01 +0200

    applying headers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,4 +1,13 @@
-export async function GET() {
-    return new Response(' Profile API data ')
+import { headers } from "next/headers";
+import { type NextRequest } from "next/server";
+
+export async function GET(req: NextRequest) {
+    const requestHeaders = new Headers(req.headers)
+    const headerList = headers()
+    console.log(headerList.get("Authorization"))
+
+    return new Response('<h1> Profile API data <h1/>', {
+        headers: { "Content-Type": "text/html" },
+    })
 }
 
