c035b9879489848ecb9eee84003d719b9c9a2d60
Yuriy Pilipenko <yurik2061@gmail.com>
2024-03-05 23:15:43 +0200

    patch request

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -12,3 +12,17 @@ export async function GET(
         headers: { "Content-Type": "application/json" },
     });
 }
+
+export async function PATCH(
+    req: Request,
+     {params}: {params: {id: string}} 
+     ) {
+
+     const body = await req.json()  
+     const {text} = body
+     const index =  comments.findIndex(c => c.id === parseInt(params.id))
+     comments[index].text = text 
+
+     return Response.json(comments[index])
+       
+}
