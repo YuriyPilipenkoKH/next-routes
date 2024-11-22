495566f7a366356dc8205c0ec4f433c500c8519c
Yuriy Pilipenko <yurik2061@gmail.com>
2024-11-22 22:24:13 +0200

    user schema added

--- src/lib/mongo.tsx
+++ src/lib/mongo.tsx
@@ -5,9 +5,11 @@ import mongoose from "mongoose"
 const connectMongoDb = async() => {
     try {
         await mongoose.connect(process.env.DATABASE_URL!)
+        console.log('connected to MongoDB')
     } 
     catch (error) {
-        
+        console.log('connection failed'+error)
+        process.exit(1)
     }
     
 }
