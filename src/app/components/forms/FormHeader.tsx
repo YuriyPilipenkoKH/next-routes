86f3df9956b31eef67f31af21bfbe56ddd619712
Yuriy Pilipenko <yurik2061@gmail.com>
2024-11-24 19:40:51 +0200

    FormHeader completed

--- src/app/components/forms/FormHeader.tsx
+++ src/app/components/forms/FormHeader.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
-const FormHeader = () => {
+interface FormHeaderProps {
+  titleLabel: string;
+}
+
+
+const FormHeader = ({titleLabel}: FormHeaderProps) => {
   return (
-    <div>FormHeader</div>
+    <div className='w-full flex flex-col space-y-1 items-center justify-center'>
+      <h2 className='text-xl font-semibold '>
+        Tracker
+      </h2>
+      <p>{titleLabel}</p>
+    </div>
   )
 }
 
