adca452a6ac1827d4314f753911fc968356011a1
Yuriy Pilipenko <yurik2061@gmail.com>
2024-11-24 11:47:16 +0200

    base form props

---
+++ src/data/formProps.ts
@@ -0,0 +1,33 @@
+import { FormBaseTypes } from "@/types/formTypes";
+
+
+export const AddNewCollectionFormProps: FormBaseTypes = {
+  formName: 'AddNewCollectionForm',
+  dimentions: ['300px', '170px','420px','250px'],
+  title: '',
+  text: '',
+};
+export const DeleteCollectionFormProps: FormBaseTypes = {
+  formName: 'DeleteCollectionForm',
+  dimentions: ['300px', '170px','420px','250px'],
+  title: '',
+  text: '',
+};
+export const AddBillFormProps: FormBaseTypes = {
+  formName: 'AddBillForm',
+  dimentions: ['280px', '290px','420px','250px'],
+  title: '',
+  text: '',
+};
+export const DeleteBillFormProps: FormBaseTypes = {
+  formName: 'DeleteBillForm',
+  dimentions: ['250px', '240px','420px','250px'],
+  title: '',
+  text: '',
+};
+export const EditBillFormProps: FormBaseTypes = {
+  formName: 'EditBillForm',
+  dimentions: ['250px', '290px','420px','250px'],
+  title: '',
+  text: '',
+};
