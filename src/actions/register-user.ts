53c1eafd476b7a6287c5868661d09311994164f3
Yuriy Pilipenko <yurik2061@gmail.com>
2024-11-25 18:21:51 +0200

    retrieve vars from formData

--- src/actions/register-user.ts
+++ src/actions/register-user.ts
@@ -3,3 +3,9 @@
 import { User } from "@/models/User"
 
 
+export const registerUser = async(formData: FormData) => {
+  const name = formData.get('name'); 
+  const email = formData.get('email'); 
+  const password = formData.get('password');
+  
+}
