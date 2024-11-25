3ad5124fbd524979d083ba740d77cacfef05aa94
Yuriy Pilipenko <yurik2061@gmail.com>
2024-11-25 16:44:14 +0200

    applied reg form

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,8 +1,11 @@
+import RegisterForm from '@/app/components/forms/RegisterForm'
 import React from 'react'
 
 const RegisterPage = () => {
   return (
-    <div>Register</div>
+    <div>
+      <RegisterForm/>
+    </div>
   )
 }
 
--- src/app/components/forms/RegisterForm.tsx
+++ src/app/components/forms/RegisterForm.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useState } from 'react'
 import FormWrapper from './FormWrapper'
 import { RegisterFormProps } from '@/data/formProps'
