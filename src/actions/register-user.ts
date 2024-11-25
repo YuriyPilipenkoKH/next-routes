d54d14f395f2ab450f8a2259cdc7b362de5150a3
Yuriy Pilipenko <yurik2061@gmail.com>
2024-11-25 18:35:01 +0200

    creating new user

--- src/actions/register-user.ts
+++ src/actions/register-user.ts
@@ -1,21 +1,36 @@
 'use server'
 
+import connectMongoDb from "@/lib/mongo";
 import { User } from "@/models/User"
+import {hashSync} from 'bcrypt-ts'
 
 
 export const registerUser = async(formData: FormData) => {
-  const name = formData.get('name'); 
-  const email = formData.get('email'); 
-  const password = formData.get('password');
+  const name = formData.get('name') as string; 
+  const email = formData.get('email') as string; 
+  const password = formData.get('password') as string;
 
     // Validation for required fields
     if (!name || !email || !password) {
-      return { success: false, error: "Name and year are required" };
+      return { success: false, error: "All fields are required" };
     }
-  
-    // Validation for data types
-    if (typeof name !== 'string' || typeof email !== 'string'|| typeof password !== 'string') {
-      return { success: false, error: "Invalid input data" };
+    try {
+      await connectMongoDb()
+      const existingUser = await User.findOne({email})
+      if (existingUser) {
+        return { success: false, error: "User already exists" };
+      }
+
+      const hashedPassword = await hashSync(password)
+      await User.create({
+        name,
+        email,
+        password: hashedPassword
+      })
+      
+    } catch (error) {
+      
     }
+
   
 }
