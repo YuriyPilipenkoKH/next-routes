286a0415e0bdbae321cae5dd25313de4614e8a79
Yuriy Pilipenko <yurik2061@gmail.com>
2024-11-25 16:32:59 +0200

    functions added

--- src/app/components/forms/RegisterForm.tsx
+++ src/app/components/forms/RegisterForm.tsx
@@ -4,6 +4,7 @@ import { RegisterFormProps } from '@/data/formProps'
 import { useForm } from 'react-hook-form'
 import { RegInput, RegisterSchema } from '@/models/auth'
 import { zodResolver } from '@hookform/resolvers/zod'
+import toast from 'react-hot-toast'
 
 const RegisterForm = () => {
   const [logError, setLogError] = useState<string>('')
@@ -27,6 +28,42 @@ const {
     isSubmitting,
     isLoading
 } = formState
+  const onSubmit = async (data: RegInput) => {
+    // if (!user) {
+    //   toast.error('User not found. Please log in.');
+    //   return;
+    // }
+    const formData = new FormData();
+    formData.append('name', data.name);
+    formData.append('email', data.email);
+    formData.append('password', data.password);
+
+
+    try {
+        // const result = await addCollection(formData);
+        // if (result.success) {
+        //     toast.success(`success`!);
+        //     reset();
+        // } else {
+        //     toast.error(`Failed : ${result.error}`);
+        // }
+      } catch 
+      (error) {
+        reset();
+        const errorMessage = error instanceof Error ? error.message : 'An unknown error occurred';
+        toast.error(`An error occurred: ${errorMessage}`);
+        setLogError(errorMessage)
+    }
+  };
+  const handleInputChange = () => {
+  if (logError) {
+    setLogError('');
+  }
+  };
+
+  const onInvalid = () => {
+  setLogError('Please fill in all required fields');
+  };
   return (
     <FormWrapper 
     titleLabel={RegisterFormProps.titleLabel}
