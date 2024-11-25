8bc193e4c2d290d5eaca32cfe08316da730d6533
Yuriy Pilipenko <yurik2061@gmail.com>
2024-11-25 18:25:00 +0200

    fields & types validation

--- src/actions/register-user.ts
+++ src/actions/register-user.ts
@@ -7,5 +7,15 @@ export const registerUser = async(formData: FormData) => {
   const name = formData.get('name'); 
   const email = formData.get('email'); 
   const password = formData.get('password');
+
+    // Validation for required fields
+    if (!name || !email || !password) {
+      return { success: false, error: "Name and year are required" };
+    }
+  
+    // Validation for data types
+    if (typeof name !== 'string' || typeof email !== 'string'|| typeof password !== 'string') {
+      return { success: false, error: "Invalid input data" };
+    }
   
 }
