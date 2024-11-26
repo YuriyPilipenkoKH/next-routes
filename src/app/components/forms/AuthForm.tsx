273070ef90be05efb8d190c935a3259868e0781c
Yuriy Pilipenko <yurik2061@gmail.com>
2024-11-26 09:25:00 +0200

    applied react-hook-form

--- src/app/components/forms/AuthForm.tsx
+++ src/app/components/forms/AuthForm.tsx
@@ -1,6 +1,9 @@
 'use client'
+import { LogInput, LoginSchema } from '@/models/auth'
 import { AuthFormBaseTypes } from '@/types/formTypes'
-import React from 'react'
+import { zodResolver } from '@hookform/resolvers/zod'
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
 
 interface AuthFormProps {
   formName: string
@@ -17,6 +20,27 @@ const AuthForm:React.FC<AuthFormProps> = ({
     backButtonHref, 
     showSocial
   } = formProps
+  const [logError, setLogError] = useState<string>('')
+  const {
+    register, 
+    handleSubmit,
+    formState,
+    reset,
+} = useForm<LogInput>({
+    defaultValues: {
+        email: '',
+        password: '',
+    },
+        mode:'all',
+        resolver: zodResolver(LoginSchema),
+})
+const {
+    errors,
+    isDirty,
+    isValid ,
+    isSubmitting,
+    isLoading
+} = formState
   return (
     <div>AuthForm</div>
   )
