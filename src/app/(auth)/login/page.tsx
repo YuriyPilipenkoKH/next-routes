cb6d83867ba63af5836285350bcce6b9930ec604
Yuriy Pilipenko <yurik2061@gmail.com>
2024-11-26 19:54:16 +0200

    redirect to dashboard

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,13 @@
 import LoginForm from '@/app/components/forms/LoginForm'
+import { getSession } from '@/lib/getSession'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
-const LoginPage = () => {
+const LoginPage = async () => {
+  const session = await getSession() 
+  if (session) {
+    redirect('/dashboard')
+  }
   return (
     <div>
       <LoginForm/>
