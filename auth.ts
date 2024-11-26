22f1eb2aa4a81c3ad211f50b50c0f6eb7826d7c2
Yuriy Pilipenko <yurik2061@gmail.com>
2024-11-26 14:54:46 +0200

    authorize

--- auth.ts
+++ auth.ts
@@ -3,6 +3,7 @@ import Credentials from 'next-auth/providers/credentials'
 import Google from "next-auth/providers/google"
 import {compare} from 'bcrypt-ts'
 import { User } from "@/models/User"
+import connectMongoDb from "@/lib/mongo"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
@@ -18,6 +19,29 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
           label: 'Password',
           type: 'string'
         },
+      },
+      authorize: async (credentials) => {
+
+        const email = credentials.email as string | undefined;
+        const password = credentials.password as string | undefined;
+
+        if (!email || !password) {
+          throw new CredentialsSignin("Please provide both email & password");
+        }
+
+        await connectMongoDb();
+
+        const user = await User.findOne({ email }).select("+password"); // Removed +role
+
+        if (!user || !user.password) {
+          throw new Error("Invalid email or password");
+        }
+
+        const isMatched = await compare(password, user.password);
+
+        if (!isMatched) {
+          throw new Error("Password did not match");
+        }
       }
 
     })
