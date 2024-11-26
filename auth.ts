50d9c00328aebb60d85884920655165e9bc91928
Yuriy Pilipenko <yurik2061@gmail.com>
2024-11-26 14:40:01 +0200

    retrieve Credentials

--- auth.ts
+++ auth.ts
@@ -1,6 +1,13 @@
-import NextAuth from "next-auth"
+import NextAuth, {CredentialsSignin} from "next-auth"
+import Credentials from 'next-auth/providers/credentials'
 import Google from "next-auth/providers/google"
+import {compare} from 'bcrypt-ts'
+import { User } from "@/models/User"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
+  providers: [
+    Google({}),
+    Credentials({})
+  ],
+ 
 })
