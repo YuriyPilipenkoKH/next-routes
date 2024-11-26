586e87ce42b80d2bc6fae9e8e5beda108c707a05
Yuriy Pilipenko <yurik2061@gmail.com>
2024-11-26 19:48:18 +0200

    action added to form

--- src/app/components/Button/SignInButton.tsx
+++ src/app/components/Button/SignInButton.tsx
@@ -1,22 +1,19 @@
 
 import React from 'react'
-import { signIn } from '../../../../auth'
 import { FcGoogle } from 'react-icons/fc'
+import { googleSignIn } from '@/actions/google-signin'
 
 const SignInButton = () => {
 
     return (
       <form
       className='w-full'
-      action={async () => {
-
-        await signIn("google");
-      }}
-  >
-  <button className='flex w-full justify-center border rounded-lg p-2 space-x-2 items-center'>
-      <p>LogIn With Google</p> <FcGoogle className='h-5 w-5' />
-  </button>
-  </form>
+      action={googleSignIn}
+      >
+      <button className='flex w-full justify-center border rounded-lg p-2 space-x-2 items-center'>
+          <p>LogIn With Google</p> <FcGoogle className='h-5 w-5' />
+      </button>
+    </form>
     )
 
 }
