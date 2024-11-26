e186803feb36c9ada60441df319d0bb20739c68c
Yuriy Pilipenko <yurik2061@gmail.com>
2024-11-26 09:22:04 +0200

    retieving props

--- src/app/components/forms/AuthForm.tsx
+++ src/app/components/forms/AuthForm.tsx
@@ -1,12 +1,22 @@
 'use client'
+import { AuthFormBaseTypes } from '@/types/formTypes'
 import React from 'react'
 
 interface AuthFormProps {
-  id: string;
-  name: string
+  formName: string
+  formProps: AuthFormBaseTypes
 }
 
-const AuthForm:React.FC<AuthFormProps> = () => {
+const AuthForm:React.FC<AuthFormProps> = ({
+  formName,
+  formProps
+}) => {
+  const {
+    titleLabel,
+    backButtonLabel, 
+    backButtonHref, 
+    showSocial
+  } = formProps
   return (
     <div>AuthForm</div>
   )
