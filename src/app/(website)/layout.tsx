20ceface41803d185117b4b75f4eec4e112070fd
Yuriy Pilipenko <yurik2061@gmail.com>
2024-11-27 23:01:59 +0200

    made logoutButton client component

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react'
+import LogoutButton from '../components/Button/LogoutButton';
 interface Props {
     children: ReactNode;
   }
@@ -6,7 +7,11 @@ interface Props {
 function Layout({ children }: Props) {
   return (
     <div className='h-full flex flex-col items-center justify-center gap-2 bg-[var(--website-bg)]'>
-        Website
+        <div className='flex'>
+          <h2>Website</h2>
+          <LogoutButton/>
+        </div>
+        
         {children}
     </div>
   )
