4b36455f807f42dfe09fce9cc888ec4a0662c69c
Yuriy Pilipenko <yurik2061@gmail.com>
2024-11-27 23:55:00 +0200

    made AuthPage async

--- src/app/(auth)/temp/page.tsx
+++ src/app/(auth)/temp/page.tsx
@@ -1,13 +1,19 @@
 import AuthForm from '@/app/components/forms/AuthForm'
 import { LoginFormProps, RegisterFormProps } from '@/data/formProps'
+import { getSession } from '@/lib/getSession'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
-const AuthPage = () => {
+const AuthPage = async() => {
+  const session = await getSession() 
+  if (session) {
+    redirect('/dashboard')
+  }
   return (
     <div>
       <AuthForm 
-      formName='registerForm'
-      formProps={RegisterFormProps}/>
+      formName='loginForm'
+      formProps={LoginFormProps}/>
     </div>
   )
 }
