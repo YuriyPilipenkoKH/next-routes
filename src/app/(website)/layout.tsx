95857f9cf2dcd842d11363f6b6a6c1e5fe876016
Yuriy Pilipenko <yurik2061@gmail.com>
2024-11-27 23:03:36 +0200

    logout works fine

--- src/app/(website)/layout.tsx
+++ src/app/(website)/layout.tsx
@@ -7,7 +7,7 @@ interface Props {
 function Layout({ children }: Props) {
   return (
     <div className='h-full flex flex-col items-center justify-center gap-2 bg-[var(--website-bg)]'>
-        <div className='flex'>
+        <div className='flex gap-2'>
           <h2>Website</h2>
           <LogoutButton/>
         </div>
