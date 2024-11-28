b7c94842082630bf76ae73a8e8ae4bf0456fd1b6
Yuriy Pilipenko <yurik2061@gmail.com>
2024-11-28 10:45:44 +0200

    definition of isRegisterErrors without using 'react-hook-form'

--- src/types/fieldErrors.ts
+++ src/types/fieldErrors.ts
@@ -4,3 +4,20 @@ type FieldErrors<T> = Partial<{
     message?: string;
   };
 }>;
+
+//---------
+type Errors<T> = Partial<{
+  [K in keyof T]: {
+    type?: string;
+    message?: string;
+  };
+}>;
+
+type LogInput = { email: string; password: string };
+type RegInput = { name: string; email: string; password: string };
+
+const isRegisterErrors = (
+  errors: Errors<LogInput | RegInput>
+): errors is Errors<RegInput> => {
+  return 'name' in errors;
+};
