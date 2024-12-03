a6ef087ba125b05868b331785acaf8e5b1871e14
Yuriy Pilipenko <yurik2061@gmail.com>
2024-12-03 08:38:06 +0200

    rewriting registerUser()

--- src/actions/register-user.ts
+++ src/actions/register-user.ts
@@ -4,13 +4,13 @@ import connectMongoDb from "@/lib/mongo";
 import { User } from "@/models/User"
 import {hashSync} from 'bcrypt-ts'
 import { revalidatePath } from "next/cache";
-import { redirect } from "next/navigation";
 
 
 export const registerUser = async(formData: FormData) => {
   const name = formData.get('name') as string; 
   const email = formData.get('email') as string; 
   const password = formData.get('password') as string;
+  const allowedEmails = process.env.ALLOWED_EMAILS?.split(',') || [];
 
     // Validation for required fields
     if (!name || !email || !password) {
@@ -18,35 +18,34 @@ export const registerUser = async(formData: FormData) => {
     }
     try {
       await connectMongoDb()
-      const existingUser = await User.findOne({email})
-      if (existingUser) {
-        return { success: false, error: "User already exists" };
-      }
-
-      const hashedPassword =  hashSync(password)
-      const newUser = await User.create({
-        name,
-        email,
-        password: hashedPassword
-      })
-
-      // Convert Mongoose document to plain object and adjust _id
-      const {password:_, _id,  __v, ...plainUser} = newUser.toObject();
-      console.log(plainUser );
-      
-      const allowedEmails = process.env.ALLOWED_EMAILS?.split(',') || [];
 
       if (allowedEmails.includes(email)) {
         console.log('Email is allowed');
+
+        const existingUser = await User.findOne({email})
+        if (existingUser) {
+          return { success: false, error: "User already exists" };
+        }
+  
+        const hashedPassword =  hashSync(password)
+        const newUser = await User.create({
+          name,
+          email,
+          password: hashedPassword
+        })
+  
+        // Convert Mongoose document to plain object and adjust _id
+        const {password:_, _id,  __v, ...plainUser} = newUser.toObject();
+        console.log(plainUser );
+
+
         revalidatePath('/login');
         return { success: true, user: plainUser };
       } else {
         console.log('Email is not allowed');
         return { success: false, error: "Email is not allowed" };
-      }
-
-
-      
+      }     
+ 
     } catch (error) {
       console.error('Error occurred while registering:', error);
       const errorMessage = error instanceof Error ? error.message : "An unexpected error occurred";
