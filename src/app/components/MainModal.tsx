dc260fabf2160a5a19bcde1316661b5bbab94674
Yuriy Pilipenko <yurik2061@gmail.com>
2024-12-15 11:04:30 +0200

    MainModal template

---
+++ src/app/components/MainModal.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface MainModalProps {
+  madaltypes: string
+}
+
+const MainModal:React.FC<MainModalProps> = ({madaltypes}) => {
+  return (
+    <div>MainModal</div>
+  )
+}
+
+export default MainModal
