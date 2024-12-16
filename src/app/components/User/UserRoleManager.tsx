44b6630092ddfffb5f3346ebfecc799640a43615
Yuriy Pilipenko <yurik2061@gmail.com>
2024-12-16 10:46:48 +0200

    retrieving props

--- src/app/components/User/UserRoleManager.tsx
+++ src/app/components/User/UserRoleManager.tsx
@@ -1,7 +1,10 @@
 import { UserRoleProps } from '@/models/User'
 import React from 'react'
 
-const UserRoleManager: React.FC<UserRoleProps> = () => {
+const UserRoleManager: React.FC<UserRoleProps> = ({ user, onRoleChange }) => {
+
+  console.log(user, onRoleChange);
+  
   return (
     <div>UserRoleManager</div>
   )
