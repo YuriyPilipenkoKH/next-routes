cffe7e76cc729611ebcec24dc1054bee14e118f2
Yuriy Pilipenko <yurik2061@gmail.com>
2024-12-17 08:59:03 +0200

    renamed AdminPage

--- src/app/(website)/adminpanel/page.tsx
+++ src/app/(website)/adminpanel/page.tsx
@@ -1,12 +1,22 @@
+import UserRoleManager from '@/app/components/User/UserRoleManager'
+import { getSession } from '@/lib/getSession'
 import React from 'react'
 
-const page = () => {
+const AdminPage =async () => {
+  const session = await getSession()
+  const user = session?.user
+  const change =() => {
+    
+  }
   return (
     <div>Admin page
 
-      
+      {/* <UserRoleManager 
+      user={user} 
+      onRoleChange ={change}
+       /> */}
     </div>
   )
 }
 
-export default page
+export default AdminPage
