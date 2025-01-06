0257fb34f67353a954573bac90b1e2b141cd5551
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-06 13:01:50 +0200

    applied useActionState hook to google SignInButton

--- src/app/components/Button/SignInButton.tsx
+++ src/app/components/Button/SignInButton.tsx
@@ -1,14 +1,15 @@
-
-import React from 'react'
+'use client'
+import React, { useActionState } from 'react'
 import { FcGoogle } from 'react-icons/fc'
 import { googleSignIn } from '@/actions/google-signin'
 
 const SignInButton = () => {
+  const [state, formAction, isPending] = useActionState(googleSignIn, undefined)
 
     return (
     <form
       className='w-full'
-      action={googleSignIn}
+      action={formAction}
       >
       <button className='flex w-full justify-center border rounded-lg p-2 space-x-2 items-center'>
           <p>LogIn With Google</p> <FcGoogle className='h-5 w-5' />
