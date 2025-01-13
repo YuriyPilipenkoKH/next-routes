a766cc7b515590af374326097191c5d4a62bb570
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-13 14:34:23 +0200

    logout toast message

--- src/app/components/Button/LogoutButton.tsx
+++ src/app/components/Button/LogoutButton.tsx
@@ -1,10 +1,14 @@
 'use client';
 import React from 'react';
 import { logoutUser } from '@/actions/logout-user';
+import toast from 'react-hot-toast';
 
 const LogoutButton = () => {
   const handleLogout = async () => {
-    await logoutUser();
+  const result=  await logoutUser();
+  if(result?.success) {
+    toast.success(result.message)
+  }
   };
 
   return (
