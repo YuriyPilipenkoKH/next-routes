d60e04b574153d507503a2732324aee1b34c7bd7
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-16 08:48:51 +0200

    importing enum

--- src/app/components/User/UserRoleManager.tsx
+++ src/app/components/User/UserRoleManager.tsx
@@ -1,14 +1,14 @@
 'use client'
-import { UserRoleProps } from '@/models/User'
+import { Roles, UserRoleProps } from '@/models/User'
 import React, { useState } from 'react'
 
 const UserRoleManager: React.FC<UserRoleProps> = ({ user, onRoleChange }) => {
 
   // console.log(user, onRoleChange);
-  const [selectedRole, setSelectedRole] = useState<"user" | "admin" | "editor">(user.role);
+  const [selectedRole, setSelectedRole] = useState<Roles>(user.role);
 
   const handleChangeRole = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const newRole = event.target.value as "user" | "admin" | "editor"; // Type assertion
+    const newRole = event.target.value as Roles; // Type assertion
     setSelectedRole(newRole);
     onRoleChange(user._id, newRole);
   };
